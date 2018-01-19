cbd3826242bd9bcdb57eb1f0cb83a09cf3190279
cladier <clement.ladier@gmail.com>
2018-01-19 17:42:19 +0100

    test from jhagg

--- get_data.py
+++ get_data.py
@@ -1,25 +1,4 @@
-import pandas as pd
+from binance.client import Client
+client = Client(api_key, api_secret)
 
-def coucou(x,y):
-    a =x+y
-    print(a)
-    return a,x,y
-
-l,m,n=coucou(63,6)
-
-def fact(n): #todo gérer le cas négatif
-    """
-    calcul le factoriel d'un nombre entier
-    :param n: int
-    :return: int
-    """
-
-    if n==1:
-        return 1
-    else:
-        return n*fact(n-1)
-
-fact(6)
-
-def fun(x):
 
